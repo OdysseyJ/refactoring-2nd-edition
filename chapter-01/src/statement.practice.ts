33e308f554e5fc131c3245b56f97a17d064c1a26
Odysseyj <odysseyj@naver.com>
2022-03-10 21:29:53 +0900

    chap01 practice

--- chapter-01/src/statement.practice.ts
+++ chapter-01/src/statement.practice.ts
@@ -1,4 +1,5 @@
-import {Invoice, Play, Plays, Performance, StatementData} from './types';
+import {Invoice, Plays, StatementData} from './types';
+import createStatementData from "./createStatementData";
 
 function usd(aNumber: number) {
   return new Intl.NumberFormat('en-US', {
@@ -8,82 +9,20 @@ function usd(aNumber: number) {
   }).format(aNumber/100);
 }
 
-export default function statement(invoice: Invoice, plays: Plays) {
-  const statementData : StatementData = {}
-  statementData.customer = invoice.customer
-  return renderPlainText(invoice, plays)
+export default function statement(invoice: Invoice, plays: Plays){
+  return renderPlainText(createStatementData(invoice, plays), plays)
 }
 
-function renderPlainText(invoice: Invoice, plays: Plays) {
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data: StatementData, plays: Plays) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
 
-  for (let perf of invoice.performances) {
-    result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
+  for (let perf of data.performances) {
+    result += `${perf.play.name} : ${usd(perf.amount)} (${
         perf.audience
     }석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance: Performance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ('comedy' === playFor(aPerformance).type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-
-    return result
-  }
-
-  function playFor(perf: Performance) {
-    const play = plays[perf.playID];
-    return play;
-  }
-
-  function amountFor(aPerformance: Performance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case 'tragedy': // 비극
-        result = 40000;
-
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
-    }
-    return result
-  }
-
 }
